96a39f562d7e3974448f5f740a41496f8efd56dc
monster1408 <yukiya.toyohara@hotmail.com>
2025-06-07 19:04:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 game = 1;
+x = 0;
 function shooting(){
-    const x = 0;
     ctx.beginPath();
     ctx.moveTo(150 + x, 200);
     ctx.lineTo(200 + x, 250);
@@ -8,11 +8,12 @@ function shooting(){
     ctx.closePath();
     ctx.fillStyle = "green";
     ctx.fill(); 
-    if(event.key){
-        x += 2;
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "ArrowRight") {
+            x += 2;
+        }
     }
-}
-
+                              }
 if(game == 1){
   setInterval(Shooting, 100);
 }
