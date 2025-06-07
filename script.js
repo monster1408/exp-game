af2b10097c87aa7f25bc0f75d135b6991529f0e6
monster1408 <yukiya.toyohara@hotmail.com>
2025-06-07 18:29:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-game = 0;
+game = 1;
 function shooting(){
-  
+    const x = 0;
+    ctx.beginPath();
+    ctx.moveTo(150 + x, 200);
+    ctx.lineTo(200 + x, 250);
+    ctx.lineTo(100 + x, 250);
+    ctx.closePath();
+    ctx.fillStyle = "green";
+    ctx.fill(); 
 }
 
 if(game == 1){
-  setInterval(Shooting, 1000);
+  setInterval(Shooting, 100);
 }
