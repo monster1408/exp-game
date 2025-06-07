f31326dac90e8ab86d7e18e5f9cfa72557b860e0
monster1408 <yukiya.toyohara@hotmail.com>
2025-06-07 19:39:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext("2d");
 let game = 1;
 let x = 0;
 function shooting(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
     ctx.beginPath();
     ctx.moveTo(150 + x, 200);
     ctx.lineTo(200 + x, 250);
@@ -13,10 +15,10 @@ function shooting(){
     ctx.fill(); 
     document.addEventListener("keydown", function(event) {
         if (event.key === "ArrowRight") {
-            x += 2;
+            x += 0.1;
         }
     });
 }
 if(game == 1){
-  setInterval(shooting, 100);
+  shooting()
 }
